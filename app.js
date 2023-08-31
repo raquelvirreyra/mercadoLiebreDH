31b8858f159a0c966e7ff8c32dee72c61eaf9268
VirreyraRuiz <raaqueeluuchaa@gmail.com>
2023-08-30 22:44:23 -0300

    configurando rutas - render

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => console.log('Servidor corriendo en el puerto '+port));
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port));
