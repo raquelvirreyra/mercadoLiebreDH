c2cca4459e25507fb1e8855cd0151c2566f99597
VirreyraRuiz <raaqueeluuchaa@gmail.com>
2023-08-30 23:02:37 -0300

    configurando hojas de estilo - render

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'views')));
-app.use(express.static(path.join(__dirname, 'public/css')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
